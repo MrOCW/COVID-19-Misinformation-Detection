7143177ca686e35af4b32d604055298bfd40735a
GlenWong97 <shuzewong@yahoo.com.sg>
2021-10-15 22:13:00 +0800

    edited main

--- ocr.py
+++ ocr.py
@@ -41,11 +41,26 @@ class OCR:
     def canny(self, image):
         return cv2.Canny(image, 100, 200)
 
-    def run(self):
-        img = cv2.imread('testocr.png')
+    def run(self, file_name):
+        img = cv2.imread(f'{file_name}')
 
         gray = self.get_grayscale(img)
         gray = self.remove_noise(gray)
 
-        custom_config = r'--oem 3 --psm 6'        
+        # 0    Orientation and script detection (OSD) only.s
+        # 1    Automatic page segmentation with OSD.
+        # 2    Automatic page segmentation, but no OSD, or OCR.
+        # 3    Fully automatic page segmentation, but no OSD. (Default)
+        # 4    Assume a single column of text of variable sizes.
+        # 5    Assume a single uniform block of vertically aligned text.
+        # 6    Assume a single uniform block of text.
+        # 7    Treat the image as a single text line.
+        # 8    Treat the image as a single word.
+        # 9    Treat the image as a single word in a circle.
+        # 10    Treat the image as a single character.
+        # 11    Sparse text. Find as much text as possible in no particular order.
+        # 12    Sparse text with OSD.
+        # 13    Raw line. Treat the image as a single text line, bypassing hacks that are Tesseract-specific.
+        custom_config = r'--oem 3 --psm 6'    
+
         return pytesseract.image_to_string(gray, config=custom_config)
